652293d3e2bc9a0c487e38a7352b349c61af80eb
Vlastilin173 <107255486+Vlastilin173@users.noreply.github.com>
2023-02-05 02:59:11 +0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -61,3 +61,18 @@ else if (n == 6)
     Console.WriteLine("Выходной день");
 else
     Console.WriteLine("Выходной день");
+
+//Если короче то так:
+Console.Clear();
+
+Console.Write("Введите день недели: ");
+int n = Convert.ToInt32(Console.ReadLine());
+while (n < 1 || n > 7)
+{
+    Console.Write("Вы ошиблись!\nВведите день недели: ");
+    n = Convert.ToInt32(Console.ReadLine());
+}
+if (n == 1 || n == 2 || n == 3 || n == 4 || n == 5)
+    Console.WriteLine("Будний день");
+else if (n == 6 || n == 7)
+    Console.WriteLine("Выходной день");
